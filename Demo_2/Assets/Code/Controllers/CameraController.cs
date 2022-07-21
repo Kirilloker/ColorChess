609483cad94b263ba14e08acc0cd289ec6dc9bac
Егор <89208912+Kirilloker@users.noreply.github.com>
2022-07-22 01:29:11 +0300

    Add Slider for Camera

--- Demo_2/Assets/Code/Controllers/CameraController.cs
+++ Demo_2/Assets/Code/Controllers/CameraController.cs
@@ -3,86 +3,80 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 using ColorChessModel;
+using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
     //Главная камера
     [SerializeField]
-    private Cinemachine.CinemachineBrain main_camera;
+    private Cinemachine.CinemachineBrain mainCamera;
 
     //Камеры
     [SerializeField]
     private List<CinemachineVirtualCamera> cinemachines;
 
-    private CameraViewType viewCamera;
-    public CameraViewType ViewCamera { get => viewCamera; set => viewCamera = value; }
+    public CameraViewType viewCamera = CameraViewType.gameStart;
 
-    private void Awake()
+    // Изменение угла во время игры
+    [SerializeField]
+    private Slider slider;
+
+    private CinemachineTrackedDolly trackInGame1;
+    private CinemachineTrackedDolly trackInGame2;
+
+
+    private void Start()
     {
         SwitchCamera();
 
-        main_camera.m_DefaultBlend.m_Time = 2f;
+        slider.value = 3.2f;
+        mainCamera.m_DefaultBlend.m_Time = 2f;
+        trackInGame1 = cinemachines[(int)CameraViewType.inGame1].GetCinemachineComponent<CinemachineTrackedDolly>();
+        trackInGame2 = cinemachines[(int)CameraViewType.inGame2].GetCinemachineComponent<CinemachineTrackedDolly>();
     }
 
     public void SwitchCamera()
     {
-        for (int i = 0; i < cinemachines.Count; i++)
+        foreach (var cinemachine in cinemachines)
         {
-            cinemachines[i].m_Priority = 1;
+            cinemachine.m_Priority = 1;
         }
 
-        switch (ViewCamera)
-        {
-            case CameraViewType.gameStart:
-                cinemachines[0].m_Priority = 2;
-                break;
-            case CameraViewType.noteMenu:
-                cinemachines[1].m_Priority = 2;
-                break;
-            case CameraViewType.inGame1:
-                cinemachines[2].m_Priority = 2;
-                break;
-            case CameraViewType.inGame2:
-                cinemachines[3].m_Priority = 2;
-                break;
-            case CameraViewType.deskMenu:
-                cinemachines[4].m_Priority = 2;
-                break;
-            default:
-                break;
-        }
+        cinemachines[(int)viewCamera].m_Priority = 10;
     }
 
-    public void SwitchCameraWithDelay(CameraViewType viewCamera)
+    public void SwitchCameraWithDelay(CameraViewType _viewCamera)
     {
-        ViewCamera = viewCamera;
+        viewCamera = _viewCamera;
         Invoke("SwitchCamera", 0.50f);
     }
 
-    //public void Change_Cameras_Rotation(float angle)
-    //{
-    //    cinemachines[2].GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition = angle;
-    //    cinemachines[3].GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition = angle;
-    //}
-    //Изменение скорости перехода между камеры
-    //public void Set_Camera_Change_Speed(float value)
-    //{
-    //    if (ChangeSettingGraphics.LowGraphics == true)
-    //    {
-    //        main_camera.m_DefaultBlend.m_Time = 0f;
-    //    }
-    //    else
-    //    {
-    //        main_camera.m_DefaultBlend.m_Time = value;
-    //    }
-    //}
+    public void RotateCamers()
+    {
+        ChangeCamersRotation(slider.value);
+    }
+    public void ChangeCamersRotation(float angle)
+    {
+        // Почему-то в Awake не получается инициализировать trackInGame, поэтому тут вот такой костыль
+        if (trackInGame1 == null && trackInGame2 == null)
+        {
+            cinemachines[(int)CameraViewType.inGame1].GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition = angle;
+            cinemachines[(int)CameraViewType.inGame2].GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition = angle;
+
+            trackInGame1 = cinemachines[(int)CameraViewType.inGame1].GetCinemachineComponent<CinemachineTrackedDolly>();
+            trackInGame2 = cinemachines[(int)CameraViewType.inGame2].GetCinemachineComponent<CinemachineTrackedDolly>();
+        }
+        else
+        {
+            trackInGame1.m_PathPosition = angle;
+            trackInGame2.m_PathPosition = angle;
+        }
+    }
+
+    public void CameraToMenu()
+    {
+        SwitchCameraWithDelay(CameraViewType.noteMenu);
+    }
+
 }
 
-public enum CameraViewType
-{
-    gameStart,
-    noteMenu,
-    inGame1,
-    inGame2,
-    deskMenu
-}
