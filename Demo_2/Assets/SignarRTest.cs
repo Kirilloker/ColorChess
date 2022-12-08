304dc7a3a898a4c8543b6f836cd04d381889b133
tealvl <70812161+tealvl@users.noreply.github.com>
2022-12-08 04:37:22 +0300

    addSignalRSupport

---
+++ Demo_2/Assets/SignarRTest.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+
+public class SignarRTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        test();
+    }
+
+    private async void test()
+    {
+        var connection = new HubConnectionBuilder()
+               .WithUrl("http://192.168.1.38:11000/Chat")
+               .Build();
+        try
+        {
+            await connection.StartAsync();
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex.Message);
+        }
+
+        await connection.InvokeAsync("SendSomeStr", "hello");
+    }
+}
