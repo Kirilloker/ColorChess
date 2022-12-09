2af5e27aa4f6ce6fca0445fe4f80465415267ca0
Егор <89208912+Kirilloker@users.noreply.github.com>
2022-12-09 11:01:20 +0300

    Add EntityFramework

---
+++ GameServer/GameServer/Database/ColorChess/GameStatistic.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class GameStatistic
+{
+    public int Id { get; set; }
+    public int Time { get; set; }
+    public int Player1Score { get; set; }
+    public int Player2Score { get; set; }
+    public DateTime Date { get; set; }
+
+
+    public int User1Id { get; set; }
+    public int User2Id { get; set; }
+    //public User Player1 { get; set; }
+    //public User Player2 { get; set; }
+}
