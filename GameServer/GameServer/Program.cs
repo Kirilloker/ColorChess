8728638ef5c3da9f780cc2c5a6faa5ee72e325aa
Егор <89208912+Kirilloker@users.noreply.github.com>
2022-12-09 12:00:10 +0300

    Change programm

--- GameServer/GameServer/Program.cs
+++ GameServer/GameServer/Program.cs
@@ -1,10 +1,71 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSignalR();
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                var accessToken = context.Request.Query["access_token"];
+
+                var path = context.HttpContext.Request.Path;
+                if (!string.IsNullOrEmpty(accessToken) &&
+                    (path.StartsWithSegments("/Game")))
+                {
+                    context.Token = accessToken;
+                }
+                return Task.CompletedTask;
+            }
+        };
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+            ValidateIssuerSigningKey = true
+        };
+    });
+
+
 
 var app = builder.Build();
 
-app.MapHub<ChatHub>("/Chat");
+
+app.MapPost("/login", () =>
+{
+    var user = "��� �� ��� ����� � ��";
+
+    //���� �� ����� ������������
+    if (user is null) return Results.Unauthorized();
+
+    var claims = new List<Claim> { new Claim(ClaimTypes.NameIdentifier, "name from db") };
+
+    var jwt = new JwtSecurityToken(
+          issuer: AuthOptions.ISSUER,
+          audience: AuthOptions.AUDIENCE,
+          claims: claims,
+          expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(20)),
+          signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+    var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+    var response = new
+    {
+        access_token = encodedJwt,
+    };
+
+    return Results.Json(response);
+});
+
+
+app.MapHub<GameHub>("/Game");
 
 
 app.Run("http://localhost:11000");
