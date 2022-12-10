2c914f9b7dfe16ba7a794233e57f8ab27c17421f
Егор <89208912+Kirilloker@users.noreply.github.com>
2022-12-10 17:54:41 +0300

    Add GameMode in Room

--- GameServer/GameServer/Database/ColorChess/Room.cs
+++ GameServer/GameServer/Database/ColorChess/Room.cs
@@ -4,6 +4,7 @@ public class Room
 {
     public int Id { get; set; }
     public string Map { get; set; }
+    public GameMode GameMode { get; set; }
     public int User1Id { get; set; }
     public int User2Id { get; set; }
 
