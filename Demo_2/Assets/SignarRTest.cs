999ae63d80459707b4961b42d9087cf738032913
Егор <89208912+Kirilloker@users.noreply.github.com>
2022-12-10 07:35:13 +0300

    Sychronize

--- Demo_2/Assets/SignarRTest.cs
+++ Demo_2/Assets/SignarRTest.cs
@@ -1,12 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Microsoft.AspNetCore.SignalR.Client;
 using System;
+using System.Net.Http;
+using System.Text.Json;
+using Newtonsoft.Json;
+using UnityEngine.Networking.Types;
+using Microsoft.AspNetCore.Connections;
 
 public class SignarRTest : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         test();
@@ -15,8 +17,25 @@ public class SignarRTest : MonoBehaviour
     private async void test()
     {
         var connection = new HubConnectionBuilder()
-               .WithUrl("http://192.168.1.38:11000/Game")
+               .WithUrl("http://192.168.1.38:11000/Game", options => 
+               {
+                   options.AccessTokenProvider = async () =>
+                   {
+                       HttpClient client = new HttpClient();
+                       HttpContent content = new StringContent("kirillok");
+                       HttpResponseMessage response = await client.PostAsync("http://192.168.1.38:11000/login", content);
+                       string contentText = await response.Content.ReadAsStringAsync();
+                       string token = JsonConvert.DeserializeObject<AccessToken>(contentText).access_token;
+                       return token;    
+                   };
+                   options.UseDefaultCredentials= true;
+               })
                .Build();
+
+        while (connection.State != HubConnectionState.Connected)
+        {
+
+        }
         try
         {
             await connection.StartAsync();
@@ -28,4 +47,20 @@ public class SignarRTest : MonoBehaviour
 
         await connection.InvokeAsync("SendSomeStr", "hello");
     }
+
+    private async void Authoriazation()
+    {
+        HttpClient httpClient = new HttpClient();
+        StringContent stringContent = new StringContent("");
+        var response = await httpClient.PostAsync("http://192.168.1.38:11000/login", stringContent);
+        Debug.Log(response);
+        string responseText = await response.Content.ReadAsStringAsync();
+
+        Debug.Log(responseText);
+
+
+        //test();
+
+    }
+        
 }
