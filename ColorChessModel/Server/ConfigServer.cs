0f00c8d43c9bd7b7870bc585947f0a146d37dc5f
Егор <89208912+Kirilloker@users.noreply.github.com>
2024-04-13 15:21:31 +0300

    Change IP config

--- ColorChessModel/Server/ConfigServer.cs
+++ ColorChessModel/Server/ConfigServer.cs
@@ -1,9 +1,9 @@
 ﻿
 public static class ConfServ
 {
-    public const string baseIP = "192.168.0.35";
+    public const string baseIP = "192.168.0.201";
     public const string basePort = "11000";
-
+    
     public const string GameServerHubUrl = "http://" + baseIP + ":" + basePort + "/Game";
     public const string LoginInUrl = "http://" + baseIP + ":" + basePort + "/login";
     public const string TopUrl = "http://" + baseIP + ":" + basePort + "/api/Info/get_top"; 
